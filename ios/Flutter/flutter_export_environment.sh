3f14a66770d2e6f30cad0c32dfb6b249f8aa5171
christiankrutsche <christian.krutsche@gmail.com>
2020-04-30 23:18:43 +0200

    updated deps

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/profiq/flutter-dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/profiq/Documents/mladez_zpevnik"
-export "FLUTTER_TARGET=/Users/profiq/Documents/mladez_zpevnik/lib/main.dart"
+export "FLUTTER_ROOT=/Users/christiankrutsche/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/christiankrutsche/Documents/christian/mladez_bystrice_flutter"
+export "FLUTTER_TARGET=/Users/christiankrutsche/Documents/christian/mladez_bystrice_flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/profiq/flutter-dev/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.3.0"
-export "FLUTTER_BUILD_NUMBER=9"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/christiankrutsche/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.4.1"
+export "FLUTTER_BUILD_NUMBER=11"
+export "TRACK_WIDGET_CREATION=true"
