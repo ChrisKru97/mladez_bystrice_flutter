2e71a544cda05906ca21b897ba1f2177b5b3a2f1
Christian Krutsche <christian.krutsche@gmail.com>
2024-11-22 13:09:37 +0100

    feat: present playlist gestures

--- lib/screens/present_playlist.dart
+++ lib/screens/present_playlist.dart
@@ -16,36 +16,57 @@ class PresentPlaylist extends StatelessWidget {
     final playlist = playlistController.getPlaylist(id);
     return Scaffold(
         appBar: AppBar(title: Text(playlist.value.name)),
-        body: ListView.builder(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            itemCount: playlist.value.songsOrder!.length,
-            itemBuilder: (context, index) {
-              final songNumber = playlist.value.songsOrder![index];
-              final song = songsController.songBox.get(songNumber);
-              if (song == null) return const Center();
-              final songText = configController.config.value.showChords
-                  ? song.withChords
-                  : song.withoutChords;
-              return Column(children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 18),
-                  child: Text('${song.number}. ${song.name}',
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Get.isDarkMode ? Colors.white : Colors.black)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 100),
-                  child: Text(songText,
-                      style: TextStyle(
-                          fontSize: song.fontSize,
-                          color: Get.isDarkMode ? Colors.white : Colors.black),
-                      textAlign: configController.config.value.alignCenter
-                          ? TextAlign.center
-                          : TextAlign.left),
-                )
-              ]);
-            }));
+        body: GestureDetector(
+            onDoubleTap: () => configController.config.update((val) {
+                  if (val == null) return;
+                  val.showChords = !val.showChords;
+                }),
+            onHorizontalDragUpdate: (details) {
+              if (details.primaryDelta != null &&
+                  (details.primaryDelta! > 15 || details.primaryDelta! < -15)) {
+                configController.config.update((val) {
+                  if (val == null) return;
+                  val.alignCenter = details.primaryDelta! > 0;
+                });
+              }
+            },
+            child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                itemCount: playlist.value.songsOrder!.length,
+                itemBuilder: (context, index) {
+                  final songNumber = playlist.value.songsOrder![index];
+                  final song = songsController.songBox.get(songNumber);
+                  if (song == null) return const Center();
+                  return Column(children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 18),
+                      child: Text('${song.number}. ${song.name}',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Get.isDarkMode
+                                  ? Colors.white
+                                  : Colors.black)),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(bottom: 100),
+                        child: Obx(() {
+                          final songText =
+                              configController.config.value.showChords
+                                  ? song.withChords
+                                  : song.withoutChords;
+                          return Text(songText,
+                              style: TextStyle(
+                                  fontSize: song.fontSize,
+                                  color: Get.isDarkMode
+                                      ? Colors.white
+                                      : Colors.black),
+                              textAlign:
+                                  configController.config.value.alignCenter
+                                      ? TextAlign.center
+                                      : TextAlign.left);
+                        }))
+                  ]);
+                })));
   }
 }
